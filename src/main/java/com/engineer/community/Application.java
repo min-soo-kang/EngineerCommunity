baf132f87ba01d25bb9d7350b6796c4755efdd0a
YoonSung Kim <opqudk@gmail.com>
2020-07-13 18:21:37 +0900

    BaseTimeEntity -> 등록수정 시간

--- src/main/java/com/engineer/community/Application.java
+++ src/main/java/com/engineer/community/Application.java
@@ -2,7 +2,9 @@ package com.engineer.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //baseTimeEntity 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
